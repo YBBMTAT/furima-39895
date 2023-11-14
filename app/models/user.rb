0bd8adfa36866442c8dd4c0305db9509223fc6f1
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-11-15 01:00:45 +0900

    ログインログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+         validates :email, format: { with: /.*\.jp\z/i }
+         validates :password, presence: true
+         validates :password, confirmation: { case_sensitive: true }
+         validates :password_confirmation, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "furimas#index"
+
+  resources :furimas, only: [:index]
+
 end
