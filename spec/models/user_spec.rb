44ae8ffb946163e730d9a57fb7b1c82250e6473e
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-11-16 23:12:24 +0900

    テストコード、新規ユーザー登録項目出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user) # Userのインスタンス生成
+  end
 
   describe 'ユーザー管理機能' do
 
-   it 'nicknameが空では登録できない'do
-    user = User.new(nickname:'', email: 'test@example.jp', password: 'a00000', password_confirmation: 'a00000')
-    user.valid?
-    expect(user.errors.full_messages).to include("Nickname can't be blank")
-   end
+   describe '新規ユーザー登録' do
 
-   it 'emailが空では登録できない'do
-    user = User.new(nickname:'test', email: '', password: 'a00000', password_confirmation: 'a00000')
-    user.valid?
-    expect(user.errors.full_messages).to include("Email can't be blank")
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ''  # nicknameの値を空にする
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+
+    it 'emailが空では登録できない' do
+      @user.email = ''  # emailの値を空にする
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    
+    it '同じ(既に登録済み)emailは登録できない'
+    end
+
+    it '@が含まれないemailは登録出来ない'
+    end
+
+    it 'パスワードが空では登録できない'
+    end
+
+    it 'パスワードが6文字以下では登録できない'
+    end
+
+    it 'パスワードが半角英数字を含まなければ登録できない'
+    end
+
+    it '確認パスワードが一致しなければ登録できない'
+    end
+
    end
+   
   end
 
 end
