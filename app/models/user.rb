971f80ff04f1ddaaa0e46c1d3cd008e28addc152
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-11-19 15:56:50 +0900

    表記内容の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
          validates :nickname, presence: true
          
-         validates :kanji_last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-         validates :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+         validates :kanji_last_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+         validates :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
          validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-         validates :password, confirmation: { case_sensitive: true }
+
 
          validates :birthday, presence: true
 end
