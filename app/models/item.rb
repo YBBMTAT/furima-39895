c832a53be29a554722a2d8660e4c675bb9a04e77
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-11-21 22:34:58 +0900

    カテゴリープルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :user
+
+    #ジャンルの選択が「---」の時は保存できないようにする
+    validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         
          validates :kanji_last_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
          validates :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
          validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-
-
          validates :birthday, presence: true
+
+         has_many :items
 end
