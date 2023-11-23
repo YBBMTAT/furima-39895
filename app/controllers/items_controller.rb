d70f7464249b67f37a538911fef531bee2642210
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-11-23 21:50:52 +0900

    保存機能、出品ボタンのリンク先設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,21 @@
 class ItemsController < ApplicationController
 
   def index
-
+    @items = Item.new
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+   @item = Item.create(item_params)
+   redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :price, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :leadtime_id,).merge(user_id: current_user.id)
   end
 
 end
