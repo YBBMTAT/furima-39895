c37d0f1e1f495ee1aa16ed1fad1271872094d29d
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-11-26 17:18:50 +0900

    商品画像有無の出品可否の設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image { Rack::Test::UploadedFile.new(Rails.root.join('test', 'test.png'), 'image/png') }
     item_name {Faker::Lorem.sentence}
     description {Faker::Lorem.sentence}
     category_id { Category.where.not(name: '---').sample.id } #「---」でないカテゴリーからランダムに1つ選ぶ
@@ -10,5 +9,10 @@ FactoryBot.define do
     leadtime_id { Leadtime.where.not(name: '---').sample.id }
     price { rand(300..9999999) } #300~9,999,999の整数をランダム生成
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,15 +14,21 @@ RSpec.describe Item, type: :model do
 
     context '出品出来ないとき' do
       it '商品画像が無ければ出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
       it '商品名が無ければ出品できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
 
-      it '商品画像が無ければ出品できない' do
-      end
-
-      it '商品画像は40文字まででないと出品できない' do
+      it '商品名は40文字まででないと出品できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
 
       it '商品説明が無ければ出品できない' do
@@ -54,6 +60,10 @@ RSpec.describe Item, type: :model do
 
       it '価格が10,000,000円以上では出品できない' do
       end
+
+      it 'ユーザーが紐付いていなければ出品できない' do
+      end
+
     end
   end
 end
