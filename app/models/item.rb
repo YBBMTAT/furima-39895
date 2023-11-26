752317a7b7d060dd1dd9f7183ab0d739321a6c44
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-11-26 21:04:36 +0900

    価格バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,6 +25,7 @@ class Item < ApplicationRecord
     validates :image, presence: true
     validates :item_name, presence: true, length: { maximum: 40 }
     validates :description, presence: true, length: { maximum: 1000 }
+    validates :price, presence: true
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
