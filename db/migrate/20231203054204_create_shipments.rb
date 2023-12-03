22784ab276d203d9c9572b65ee6650afe9a44f5e
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-12-03 17:43:49 +0900

    購入商品の表示

--- db/migrate/20231203054204_create_shipments.rb
+++ db/migrate/20231203054204_create_shipments.rb
@@ -2,6 +2,14 @@ class CreateShipments < ActiveRecord::Migration[7.0]
   def change
     create_table :shipments do |t|
 
+      t.string :address,                  null: false
+      t.integer :prefecture_id,           null: false
+      t.string :ctiy,                     null: false
+      t.string :street_num,               null: false
+      t.string :building,                 null: false
+      t.string :phone,                    null: false
+      t.string :buy,                      null: false,foreign_key: true
+      
       t.timestamps
     end
   end
