297422b46278707ad5459ac15cd895dae3258883
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-12-17 18:32:21 +0900

    edit_move_to_top_sessionの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update, :show]
   before_action :move_to_new_user_session, except: [:index,:show]
-  before_action :edit_move_to_top_session, except: [:index,:show,:new,:destroy]
+  before_action :edit_move_to_top_session, only: [:edit, :update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -58,7 +58,7 @@ class ItemsController < ApplicationController
   end
 
   def edit_move_to_top_session
-    unless current_user == @item.user && !Buy.exists?(item_id: @item.id)
+    if current_user != @item.user || Buy.exists?(item_id: @item.id)
      redirect_to root_path
     end
   end
