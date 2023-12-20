d7e303ed68daf99aa61b0bbfe52aebb21aaa1d10
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-12-21 00:55:36 +0900

    js試作１

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,20 +1,23 @@
-window.addEventListener('turbo:load', () => {
+document.addEventListener('turbo:load', () => {
+  console.log("DOM content loaded");
+ const priceInput = document.getElementById('item-price'); // 金額を入力した数値をpriceInputという変数に格納する
+ const addTaxDom = document.getElementById('add-tax-price'); // 販売手数料の数値をaddTaxDomという変数に格納する
+ const profitDom = document.getElementById('profit'); // 販売利益の数値をprofitDomという変数に格納する
 
-// 金額を入力した数値をpriceInputという変数に格納する
-const priceInput = document.getElementById('item-price');
-//クリックする度イベント発火
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
+ const updateResults = () => {
+  const inputValue = priceInput.value;//価格入力
+  const calculatedTax = isNaN(inputValue) ? "" : Math.floor(inputValue * 0.1);//手数料計算
+  const calculatedProfit = isNaN(inputValue) ? "" : inputValue - calculatedTax;//利益計算
+ 
+  //計算結果表示
+  addTaxDom.textContent = calculatedTax;
+  profitDom.textContent = calculatedProfit;
+ };
+  //イベント発火
+  priceInput.addEventListener("input", () => {
+    console.log("Input event detected");
+    updateResults(); //計算結果更新
+  });
 
-
-// 販売手数料の数値をaddTaxDomという変数に格納する
-const addTaxDom = document.getElementById('add-tax-price');
-//入力した金額をもとに販売手数料(10%)を計算する処理、全角時のNaNの非表示
-addTaxDom.innerHTML = isNaN(inputValue) ? "" : Math.floor(inputValue * 0.1);
-
-// 販売利益の数値をprofitDomという変数に格納する
-const profitDom = document.getElementById('profit');
-//入力した金額をもとに利益を計算する処理、全角時のNaNの非表示
-profitDom.innerHTML = isNaN(inputValue) ? "" : inputValue - addTaxDom.innerHTML;
-})
+  updateResults();//計算結果更新
 });
