cef829bbf2cbfe6fe4f67d7bc11035fadf804cc3
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-12-21 20:48:43 +0900

    レンダー時イベント発火条件追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,16 @@
-document.addEventListener('turbo:load', () => {
+const price = () => {
   console.log("DOM content loaded");
- const priceInput = document.getElementById('item-price'); // 金額を入力した数値をpriceInputという変数に格納する
- const addTaxDom = document.getElementById('add-tax-price'); // 販売手数料の数値をaddTaxDomという変数に格納する
- const profitDom = document.getElementById('profit'); // 販売利益の数値をprofitDomという変数に格納する
+ const priceInput = document.getElementById('item-price'); // 金額を入力した数値をpriceInputという変数に格納
+ const addTaxDom = document.getElementById('add-tax-price'); // 販売手数料の数値をaddTaxDomという変数に格納
+ const profitDom = document.getElementById('profit'); // 販売利益の数値をprofitDomという変数に格納
 
- const updateResults = () => {
+   //イベント発火
+  priceInput.addEventListener("input", () => {
+    Results(); //計算
+  });
+
+ //計算
+ const Results = () => {
   const inputValue = priceInput.value;//価格入力
   const calculatedTax = isNaN(inputValue) ? "" : Math.floor(inputValue * 0.1);//手数料計算
   const calculatedProfit = isNaN(inputValue) ? "" : inputValue - calculatedTax;//利益計算
@@ -13,11 +19,8 @@ document.addEventListener('turbo:load', () => {
   addTaxDom.textContent = calculatedTax;
   profitDom.textContent = calculatedProfit;
  };
-  //イベント発火
-  priceInput.addEventListener("input", () => {
-    console.log("Input event detected");
-    updateResults(); //計算結果更新
-  });
 
-  updateResults();//計算結果更新
-});
+};
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
+
