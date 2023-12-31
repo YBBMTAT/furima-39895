07ee10cd80c60644e0d3242e55ca43758f25e728
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2023-12-31 23:06:28 +0900

    ja.ymlの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,19 +10,19 @@ class Item < ApplicationRecord
     has_one :buy
     
     #カテゴリーの選択が「---」の時は保存できないようにする
-    validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :category_id, numericality: { other_than: 1, message: "を選択してください"}
 
     #状態の選択が「---」の時は保存できないようにする
-    validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :condition_id, numericality: { other_than: 1, message: "を選択してください"}
 
     #発送日の選択が「---」の時は保存できないようにする
-    validates :leadtime_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :leadtime_id, numericality: { other_than: 1, message: "を選択してください"}
 
     #送料の選択が「---」の時は保存できないようにする
-    validates :postage_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :postage_id, numericality: { other_than: 1, message: "を選択してください"}
 
     #地域の選択が「---」の時は保存できないようにする
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください"}
 
     validates :image, presence: true
     validates :item_name, presence: true, length: { maximum: 40 }
