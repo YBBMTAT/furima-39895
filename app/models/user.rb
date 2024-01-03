a2c2da6586499a2722131c4bb062d5522bea997a
YBBMTAT <forsterite_0829_my@yahoo.co.jp>
2024-01-03 17:25:50 +0900

    エラーメッセージの翻訳によるテストコードの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上の半角英数字で入力してください。"}
          validates :nickname, presence: true
-         validates :kanji_last_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-         validates :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-         validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-         validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+         validates :kanji_last_name, presence: true, format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角漢字平仮名で入力してください。"}
+         validates :kanji_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角漢字平仮名で入力してください。"}
+         validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください。" }
+         validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください。"}
          validates :birthday, presence: true
 
          has_many :items
